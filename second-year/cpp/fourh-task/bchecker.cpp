48fedf1004e02963d4f60e8d33e95cbc52d73f12
Nikita Okhotnikov <nikiquark@gmail.com>
2019-09-17 11:45:22 +0700

    Fix fourth task

--- second-year/cpp/fourh-task/bchecker.cpp
+++ second-year/cpp/fourh-task/bchecker.cpp
@@ -4,24 +4,15 @@
 bool BraceChecker::isBalanced(std::string& s) {
 	std::stack<char> a;
 	for (int i = 0; i < s.length(); i++) {
-		if (a.empty()) {
-			a.push(s[i]);
+		if (!a.empty()) {
+			if ((s[i] == ')' && a.top() == '(') ||
+				(s[i] == '}' && a.top() == '{') ||
+				(s[i] == ']' && a.top() == '[')){
+				a.pop();
+				continue;
+			}
 		}
-		else {
-			if (s[i] == ')' && a.top() == '(') {
-				a.pop();
-				continue;
-			}
-			if (s[i] == '}' && a.top() == '{') {
-				a.pop();
-				continue;
-			}
-			if (s[i] == ']' && a.top() == '[') {
-				a.pop();
-				continue;
-			}
-			a.push(s[i]);
-		}
+		a.push(s[i]);
 	}
 	return a.empty();
 }
